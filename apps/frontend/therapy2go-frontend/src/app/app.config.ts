531a53a798aaf0b00d6427aed51162a335979dbd
Navin Tamilchelvam <navin.tamilchelvam89@gmail.com>
2025-04-01 14:13:28 +0200

    mastermind::add func declarations for animations

--- apps/frontend/therapy2go-frontend/src/app/app.config.ts
+++ apps/frontend/therapy2go-frontend/src/app/app.config.ts
@@ -1,15 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay, } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    importProvidersFrom(BrowserAnimationsModule),
   ],
 };
