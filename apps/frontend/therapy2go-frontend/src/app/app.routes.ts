4009c1b93ccf0f67ee3af78222c4e2a7e93c7840
Navin Tamilchelvam <navin.tamilchelvam89@gmail.com>
2025-04-04 15:11:27 +0200

    mastermind::add home component to project and refactor app.routes.ts and styles.scss

--- apps/frontend/therapy2go-frontend/src/app/app.routes.ts
+++ apps/frontend/therapy2go-frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Route } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: NxWelcomeComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: 'therapienow-products',
---
+++ apps/frontend/therapy2go-frontend/src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home.component',
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
