573ad48b032ce88b024f1f466f698090615b4b8a
Navin Tamilchelvam <navin.tamilchelvam89@gmail.com>
2025-04-12 16:41:05 +0200

    mastermind::frontend::remove unnecessary imports in app.config.ts, refactor deployment script and refactor name in api.service.ts

--- apps/frontend/therapy2go-frontend/src/app/app.config.ts
+++ apps/frontend/therapy2go-frontend/src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideClientHydration, withEventReplay, } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,7 +19,19 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      BrowserModule,
+      // Angular Material
+      MatToolbarModule,
+      MatButtonModule,
+      MatCardModule,
+      MatIconModule,
+      MatDividerModule,
+      MatListModule,
+      // Bootstrap
+      NgbCollapseModule,
+    ),
     provideHttpClient()
   ],
 };
