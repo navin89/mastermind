d071de2bc823d4104fa4c854c9a5db778fc14f96
Navin Tamilchelvam <navin.tamilchelvam89@gmail.com>
2025-04-23 16:11:06 +0200

    mastermind::frontend::add ssr setup codes, import zone js in main.server.ts and add static routes in app.routes.server.ts

--- apps/frontend/therapy2go-frontend/src/app/app.routes.server.ts
+++ apps/frontend/therapy2go-frontend/src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'services',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'therapienow-form',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server,
   },
 ];
